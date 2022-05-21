ad236c94d7392c0f3670a5fc1857719a1d0c31fb
BianorHolanda <bianorholanda@hotmail.com>
2022-05-21 17:17:16 -0300

    First Commit

---
+++ src/First/Mundo.java
@@ -0,0 +1,18 @@
+package First;
+
+public class Mundo {
+	private String nome;
+	
+	public Mundo(String nome) {
+		
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+}
---
+++ src/First/PrimeiroCodigoDio.java
@@ -0,0 +1,10 @@
+package First;
+
+public class PrimeiroCodigoDio {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
